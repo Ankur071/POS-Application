e018c600e8cf966877762d599a4ffa885c55a797
Ankur071 <ankuryadav7559@gmail.com>
2025-12-22 12:27:40 +0530

    feat: create category APIs and product APIs

---
+++ pos-backend/src/main/java/com/zosh/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.zosh.service;
+
+import com.zosh.mapper.UserMapper;
+import com.zosh.modal.User;
+import com.zosh.payload.dto.ProductDTO;
+
+import java.util.List;
+
+public interface ProductService {
+
+    ProductDTO createProduct(ProductDTO productDTO, User user) throws Exception;
+    ProductDTO updateProduct(Long id, ProductDTO productDTO, User user) throws Exception;
+    void deleteProduct(Long id, User user) throws Exception;
+    List<ProductDTO> getProductsByStoreId(Long storeId);
+    List<ProductDTO> searchByKeyword(Long storeId, String keyword);
+
+}
